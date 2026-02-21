3ec05647b9b8fb17e5929019a3a33a17bcc71a73
Samay Samrat <samaysamrat64@gmail.com>
2026-02-22 00:18:42 +0530

    Initial Streamlit App

--- model.py
+++ model.py
@@ -4,25 +4,21 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 def train_model():
-    # Load the valid csv file
     df = pd.read_csv("netflix_customer_churn.csv")
 
-    # Drop unnecessary columns
     if 'customer_id' in df.columns:
         df = df.drop('customer_id', axis=1)
 
     X = df.drop("churned", axis=1)
     y = df["churned"]
 
-    # One hot encode categorical features using get_dummies
     X = pd.get_dummies(X, drop_first=True)
 
-    # Train test split
+
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42, stratify=y
     )
 
-    # Train Tuned Decision Tree
     model = DecisionTreeClassifier(
         max_depth=10, 
         min_samples_leaf=2, 
@@ -31,10 +27,8 @@ def train_model():
     )
     model.fit(X_train, y_train)
 
-    # Predict
     y_pred = model.predict(X_test)
 
-    # Evaluate metrics
     metrics = {
         "accuracy": accuracy_score(y_test, y_pred),
         "precision": precision_score(y_test, y_pred),
